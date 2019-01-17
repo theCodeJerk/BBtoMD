3ec8decbbaacb266a8e8738b0b74fef15a39737d
Houston McClung <hmcclungiii@gmail.com>
2019-01-16 22:43:41 -0500

    added all files

---
+++ bbtomd/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JiphyConfig(AppConfig):
+    name = 'jiphy'
--- bbtomd/urls.py
+++ bbtomd/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from bbtomd.views import ConverterView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', ConverterView.as_view()),
 ]
